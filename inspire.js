09065478330820fb4efe510e308b14a9ac91be07
Holly Henske <hcover333@gmail.com>
2024-11-28 14:50:12 -0600

    corrected typo

--- inspire.js
+++ inspire.js
@@ -1,5 +1,5 @@
 const verseArray = [
-  "Trust in the Lord with all thine heart; a;nd lean not unto thineown understanding. -Proverbs 3:5",
+  "Trust in the Lord with all thine heart; and lean not unto thineown understanding. -Proverbs 3:5",
   "So do not fear, for I am with you; do not be dismayed, for I am your God. I will strengthen you and help you; I will uphold you with my righteous righ hand. -Issaiah 41:10",
   "Those who hope in the Lord will renew their strength. Isaiah 40:31",
   "God is our refuge and strength, a very present help in trouble therefore we will not fear. -Psalm 46:1",
